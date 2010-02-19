1219689e0c3da420a633b8bfeb3d902c02aff41a
eduzera <eduzera@eduzera-laptop.(none)>
2010-02-19 16:54:21 -0200

    site jr completo

--- jr_primeiros_dados.sql
+++
@@ -1,6 +0,0 @@
-INSERT INTO type_insurances (id,name,description,url_img,created_at,updated_at) VALUES
- (1,'AUTOMÓVEIS','It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout','../images/seguro_automovel.jpg','2010-01-29 20:42:23','2010-01-30 15:39:53'),
- (2,'SAUDE','It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout','../images/seguro_saude.jpg','2010-01-29 20:43:06','2010-01-29 20:43:06'),
- (3,'RESIDENCIAL','It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout','../images/seguro_residencial.jpg','2010-01-29 20:43:35','2010-01-29 20:43:35'),
- (4,'VIDA','It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout','../images/seguro_vida.jpg','2010-01-29 20:44:00','2010-01-29 20:44:00');
-

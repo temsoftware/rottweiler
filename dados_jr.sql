e4a3516a0129cdce557f19e8a074738bed17d98f
Eduardo Zaghi <eduzera@Eduardo-Zaghis-MacBook-Pro.local>
2010-07-18 17:03:39 -0300

    mudando dados_jr

--- dados_jr.sql
+++ dados_jr.sql
@@ -1,52 +1,51 @@
-USE hottweiler_development;
-INSERT INTO `hottweiler_development`.`aboutjrs` (`id`,`description`,`created_at`,`updated_at`) VALUES
+INSERT INTO `hottweiler`.`aboutjrs` (`id`,`description`,`created_at`,`updated_at`) VALUES
   (1,'No máximo até o final da manhã desta terça-feira, o lateral-direito Cicinho será anunciado como novo reforço do São Paulo para a temporada 2010.\n\nNo máximo até o final da manhã desta terça-feira, o lateral-direito Cicinho será anunciado como novo reforço do São Paulo para a temporada 2010\n\n<h1>São Paulo x Monterrey: ingressos à venda para a estreia tricolor na Libertadores</h1>','2010-02-08 20:48:39','2010-02-08 20:48:39');
-INSERT INTO `hottweiler_development`.`contacts` (`id`,`name`,`email`,`tel`,`news`,`created_at`,`updated_at`) VALUES
+INSERT INTO `hottweiler`.`contacts` (`id`,`name`,`email`,`tel`,`news`,`created_at`,`updated_at`) VALUES
   (1,'Eduardo Zaghi','eduzera@gmail.com','33995786',0,'2010-02-10 18:19:03','2010-02-10 18:22:29');
-INSERT INTO `hottweiler_development`.`insurance_businesses` (`id`,`name`,`description`,`img_titulo`,`span_name_id`,`insurance_id`,`created_at`,`updated_at`,`type`) VALUES
+INSERT INTO `hottweiler`.`insurance_businesses` (`id`,`name`,`description`,`img_titulo`,`span_name_id`,`insurance_id`,`created_at`,`updated_at`,`type`) VALUES
   (1,'SEGUROS EMPRESARIAS','TESTE\n\n\nTESTE\n\nEDU\n\nEDU\n\nMKAUM','../images/seg_empresarial_titulo.jpg','seg_emp_conteudo_fundo',4,'2010-02-03 19:46:18','2010-02-03 23:15:01',NULL);
-INSERT INTO `hottweiler_development`.`insurance_businesses` (`id`,`name`,`description`,`img_titulo`,`span_name_id`,`insurance_id`,`created_at`,`updated_at`,`type`) VALUES
+INSERT INTO `hottweiler`.`insurance_businesses` (`id`,`name`,`description`,`img_titulo`,`span_name_id`,`insurance_id`,`created_at`,`updated_at`,`type`) VALUES
   (2,'SEGUROS PESSOAIS','Seguros Pessoais\n\nTESTe\n\nTESTE\n\nEDU','../images/seg_pessoal_titulo.jpg','seg_pess_contuedo_fundo',3,'2010-02-03 19:47:26','2010-02-03 23:13:31',NULL);
-INSERT INTO `hottweiler_development`.`insurance_businesses` (`id`,`name`,`description`,`img_titulo`,`span_name_id`,`insurance_id`,`created_at`,`updated_at`,`type`) VALUES
+INSERT INTO `hottweiler`.`insurance_businesses` (`id`,`name`,`description`,`img_titulo`,`span_name_id`,`insurance_id`,`created_at`,`updated_at`,`type`) VALUES
   (3,'TAXI','A sete dias do começo das Olimpíadas de Inverno, Vancouver não tem neve suficiente para a realização de várias competições. A maior preocupação é na Montanha Cypress, palco de seis modalidades, que sofre com temperaturas mais altas do que o esperado.\n\n\nEnquanto em Whistler, outra cidade-sede dos Jogos, a queda de neve bate recordes e chega a dez metros de altura, o comitê organizador canadense usa caminhões e helicópteros em Vancouver para transportar neve dos pontos mais altos até os locais de competição.\n\n\nOs treinos em Vancouver foram transferidos para Whistler, e a estação de esqui da Montanha Cypress foi fechada ao público duas semanas antes do previsto. A medida foi tomada para que as pistas estejam em perfeitas condições durante as Olimpíadas.','../images/seg_pessoal_taxi_titulo.jpg','seg_pess_conteudo_fundo_taxi',1,'2010-02-03 19:53:25','2010-02-05 21:24:14',NULL);
-INSERT INTO `hottweiler_development`.`insurance_businesses` (`id`,`name`,`description`,`img_titulo`,`span_name_id`,`insurance_id`,`created_at`,`updated_at`,`type`) VALUES
+INSERT INTO `hottweiler`.`insurance_businesses` (`id`,`name`,`description`,`img_titulo`,`span_name_id`,`insurance_id`,`created_at`,`updated_at`,`type`) VALUES
   (4,'Automovel','A Arena Barueri certamente ficará lotada neste domingo, quando São Paulo e Santos farão o clássico da sétima rodada do Campeonato Paulista. Até o fechamento das bilheterias desta sexta, 9.301 ingressos dos 25.832 colocados à venda já foram comercializados.\n\n \n\nAs bilheterias voltarão a abrir na manhã deste sábado. Veja abaixo os postos de venda e os preços dos ingressos. Vale lembrar que no domingo, as bilheterias da Arena Barueri só ficarão abertas até às 13h.\n\n \n\nOS PREÇOS\n- Arquibancada: R$ 40 (meia-entrada R$ 20)\n\n- Arquibancada Sócio Torcedor: R$ 15 (vendas no Morumbi, Arena Barueri e www.futebolcard.com)\n\n- Numeradas: R$ 60 (meia-entrada R$ 30)\n\n- Arquibancadas para torcida do Santos: R$ 40 (meia-entrada R$ 20) ','../images/seg_pessoal_aut_titulo.jpg','seg_pess_conteudo_fundo_auto',1,'2010-02-03 19:53:53','2010-02-05 21:29:20',NULL);
-INSERT INTO `hottweiler_development`.`insurance_businesses` (`id`,`name`,`description`,`img_titulo`,`span_name_id`,`insurance_id`,`created_at`,`updated_at`,`type`) VALUES
+INSERT INTO `hottweiler`.`insurance_businesses` (`id`,`name`,`description`,`img_titulo`,`span_name_id`,`insurance_id`,`created_at`,`updated_at`,`type`) VALUES
   (5,'Consorcio',NULL,'../images/seg_pessoal_consorcio_titulo.jpg','seg_pess_conteudo_fundo_consorcio',1,'2010-02-03 19:54:06','2010-02-03 20:17:15',NULL);
-INSERT INTO `hottweiler_development`.`insurance_businesses` (`id`,`name`,`description`,`img_titulo`,`span_name_id`,`insurance_id`,`created_at`,`updated_at`,`type`) VALUES
+INSERT INTO `hottweiler`.`insurance_businesses` (`id`,`name`,`description`,`img_titulo`,`span_name_id`,`insurance_id`,`created_at`,`updated_at`,`type`) VALUES
   (6,'Fiança Locatícia',NULL,'../images/seg_pessoal_fianca_titulo.jpg','seg_pess_conteudo_fundo_fianca',1,'2010-02-03 19:54:28','2010-02-03 20:20:33',NULL);
-INSERT INTO `hottweiler_development`.`insurance_businesses` (`id`,`name`,`description`,`img_titulo`,`span_name_id`,`insurance_id`,`created_at`,`updated_at`,`type`) VALUES
+INSERT INTO `hottweiler`.`insurance_businesses` (`id`,`name`,`description`,`img_titulo`,`span_name_id`,`insurance_id`,`created_at`,`updated_at`,`type`) VALUES
   (7,'Previdencia Privade',NULL,'../images/seg_pessoal_previdencia_titulo.jpg','seg_pess_conteudo_fundo_privada',1,'2010-02-03 19:54:47','2010-02-03 20:17:29',NULL);
-INSERT INTO `hottweiler_development`.`insurance_businesses` (`id`,`name`,`description`,`img_titulo`,`span_name_id`,`insurance_id`,`created_at`,`updated_at`,`type`) VALUES
+INSERT INTO `hottweiler`.`insurance_businesses` (`id`,`name`,`description`,`img_titulo`,`span_name_id`,`insurance_id`,`created_at`,`updated_at`,`type`) VALUES
   (8,'Residencia',NULL,'../images/seg_pessoal_residencia_titulo.jpg','seg_pess_conteudo_fundo_residencia',1,'2010-02-03 19:55:02','2010-02-03 20:17:34',NULL);
-INSERT INTO `hottweiler_development`.`insurance_businesses` (`id`,`name`,`description`,`img_titulo`,`span_name_id`,`insurance_id`,`created_at`,`updated_at`,`type`) VALUES
+INSERT INTO `hottweiler`.`insurance_businesses` (`id`,`name`,`description`,`img_titulo`,`span_name_id`,`insurance_id`,`created_at`,`updated_at`,`type`) VALUES
   (9,'Vida',NULL,'../images/seg_pessoal_vida_titulo.jpg','seg_pess_conteudo_fundo_vida',1,'2010-02-03 19:55:10','2010-02-03 20:17:21',NULL);
-INSERT INTO `hottweiler_development`.`insurance_businesses` (`id`,`name`,`description`,`img_titulo`,`span_name_id`,`insurance_id`,`created_at`,`updated_at`,`type`) VALUES
+INSERT INTO `hottweiler`.`insurance_businesses` (`id`,`name`,`description`,`img_titulo`,`span_name_id`,`insurance_id`,`created_at`,`updated_at`,`type`) VALUES
   (10,'Saúde',NULL,'../images/seg_empresarial_saude_titulo.jpg','seg_emp_conteudo_fundo_saude',2,'2010-02-03 19:55:24','2010-02-03 21:54:47',NULL);
-INSERT INTO `hottweiler_development`.`insurance_businesses` (`id`,`name`,`description`,`img_titulo`,`span_name_id`,`insurance_id`,`created_at`,`updated_at`,`type`) VALUES
+INSERT INTO `hottweiler`.`insurance_businesses` (`id`,`name`,`description`,`img_titulo`,`span_name_id`,`insurance_id`,`created_at`,`updated_at`,`type`) VALUES
   (11,'Ondotológico',NULL,'../images/seg_empresarial_odonto_titulo.jpg','seg_emp_conteudo_fundo_odontologico',2,'2010-02-03 19:55:39','2010-02-03 21:55:44',NULL);
-INSERT INTO `hottweiler_development`.`insurance_businesses` (`id`,`name`,`description`,`img_titulo`,`span_name_id`,`insurance_id`,`created_at`,`updated_at`,`type`) VALUES
+INSERT INTO `hottweiler`.`insurance_businesses` (`id`,`name`,`description`,`img_titulo`,`span_name_id`,`insurance_id`,`created_at`,`updated_at`,`type`) VALUES
   (12,'Vida',NULL,'../images/vida_empresarial_saude_titulo.jpg','seg_emp_conteudo_fundo_vida',2,'2010-02-03 19:55:58','2010-02-03 21:56:37',NULL);
-INSERT INTO `hottweiler_development`.`insurance_businesses` (`id`,`name`,`description`,`img_titulo`,`span_name_id`,`insurance_id`,`created_at`,`updated_at`,`type`) VALUES
+INSERT INTO `hottweiler`.`insurance_businesses` (`id`,`name`,`description`,`img_titulo`,`span_name_id`,`insurance_id`,`created_at`,`updated_at`,`type`) VALUES
   (13,'Previdencia Privada',NULL,'../images/seg_empresarial_previdencia_titulo.jpg','seg_emp_conteudo_fundo_previdencia',2,'2010-02-03 19:56:14','2010-02-03 21:57:19',NULL);
-INSERT INTO `hottweiler_development`.`insurance_businesses` (`id`,`name`,`description`,`img_titulo`,`span_name_id`,`insurance_id`,`created_at`,`updated_at`,`type`) VALUES
+INSERT INTO `hottweiler`.`insurance_businesses` (`id`,`name`,`description`,`img_titulo`,`span_name_id`,`insurance_id`,`created_at`,`updated_at`,`type`) VALUES
   (14,'Saude',NULL,'../images/seg_pessoal_saude_titulo.jpg','seg_pess_conteudo_fundo_saude',1,'2010-02-03 20:11:46','2010-02-03 20:16:40',NULL);
-INSERT INTO `hottweiler_development`.`insurances` (`id`,`name`,`created_at`,`updated_at`) VALUES
+INSERT INTO `hottweiler`.`insurances` (`id`,`name`,`created_at`,`updated_at`) VALUES
   (1,'Pessoais','2010-02-02 20:43:50','2010-02-02 20:43:50');
-INSERT INTO `hottweiler_development`.`insurances` (`id`,`name`,`created_at`,`updated_at`) VALUES
+INSERT INTO `hottweiler`.`insurances` (`id`,`name`,`created_at`,`updated_at`) VALUES
   (2,'Empresariais','2010-02-02 20:43:57','2010-02-02 20:43:57');
-INSERT INTO `hottweiler_development`.`insurances` (`id`,`name`,`created_at`,`updated_at`) VALUES
+INSERT INTO `hottweiler`.`insurances` (`id`,`name`,`created_at`,`updated_at`) VALUES
   (3,'Pessoais Principal','2010-02-03 23:12:57','2010-02-03 23:12:57');
-INSERT INTO `hottweiler_development`.`insurances` (`id`,`name`,`created_at`,`updated_at`) VALUES
+INSERT INTO `hottweiler`.`insurances` (`id`,`name`,`created_at`,`updated_at`) VALUES
   (4,'Empresariais Principal','2010-02-03 23:13:08','2010-02-03 23:13:08');
-INSERT INTO `hottweiler_development`.`stores` (`id`,`name`,`street`,`number`,`neighborhood`,`cep`,`city`,`state`,`telephone`,`created_at`,`updated_at`,`fax`) VALUES
+INSERT INTO `hottweiler`.`stores` (`id`,`name`,`street`,`number`,`neighborhood`,`cep`,`city`,`state`,`telephone`,`created_at`,`updated_at`,`fax`) VALUES
   (1,'Loja JR','Joaquim Campos',1119,'Pari',NULL,'São Paulo','SP','11 6698-6758','2010-02-10 19:19:43','2010-02-10 19:44:47','11 2695-1394');
-INSERT INTO `hottweiler_development`.`type_insurances` (`id`,`name`,`description`,`url_img`,`created_at`,`updated_at`) VALUES
+INSERT INTO `hottweiler`.`type_insurances` (`id`,`name`,`description`,`url_img`,`created_at`,`updated_at`) VALUES
   (1,'AUTOMÓVEIS','It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout','../images/seguro_automovel.jpg','2010-01-29 20:42:23','2010-01-30 15:39:53');
-INSERT INTO `hottweiler_development`.`type_insurances` (`id`,`name`,`description`,`url_img`,`created_at`,`updated_at`) VALUES
+INSERT INTO `hottweiler`.`type_insurances` (`id`,`name`,`description`,`url_img`,`created_at`,`updated_at`) VALUES
   (2,'SAUDE','It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout','../images/seguro_saude.jpg','2010-01-29 20:43:06','2010-01-29 20:43:06');
-INSERT INTO `hottweiler_development`.`type_insurances` (`id`,`name`,`description`,`url_img`,`created_at`,`updated_at`) VALUES
+INSERT INTO `hottweiler`.`type_insurances` (`id`,`name`,`description`,`url_img`,`created_at`,`updated_at`) VALUES
   (3,'RESIDENCIAL','It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout','../images/seguro_residencial.jpg','2010-01-29 20:43:35','2010-01-29 20:43:35');
-INSERT INTO `hottweiler_development`.`type_insurances` (`id`,`name`,`description`,`url_img`,`created_at`,`updated_at`) VALUES
+INSERT INTO `hottweiler`.`type_insurances` (`id`,`name`,`description`,`url_img`,`created_at`,`updated_at`) VALUES
   (4,'VIDA','It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout','../images/seguro_vida.jpg','2010-01-29 20:44:00','2010-01-29 20:44:00');
 

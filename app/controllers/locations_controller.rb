89a2543f3b456b68c6b71f1901de550b59d472e8
Eduardo Zaghi <eduzera@Eduardo-Zaghis-MacBook-Pro.local>
2010-07-18 18:10:52 -0300

    mudanca no locations e alguns erros

--- app/controllers/locations_controller.rb
+++ app/controllers/locations_controller.rb
@@ -6,14 +6,6 @@ class LocationsController < ApplicationController
 
     @store = Store.first
 
-    @map = GMap.new("map_div_id")
-    @map.control_init(:large_map => true, :map_type => true)
-    @map.center_zoom_init([-23.526789,-46.607373], 16)
-
-    marker = GMarker.new([-23.526789,-46.607373],
-      :title => "#{@store.name}", :info_window => "#{@store.name}: <br> Rua #{@store.street}, #{@store.number} - #{@store.neighborhood} <br> #{@store.city} - #{@store.state}")
-    @map.overlay_init(marker)
-
   end
 
 end
